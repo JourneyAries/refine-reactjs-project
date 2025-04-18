2c595e84d1a7f4df13af3a0a450d501d6c266bbe
JourneyAries <journeyaries13@gmail.com>
2025-04-18 23:32:37 +0700

    [public-holidays-app](v1): fetching 100%

--- public-holidays-app/src/App.jsx
+++ public-holidays-app/src/App.jsx
@@ -1,7 +1,34 @@
 import { PiCaretDown } from 'react-icons/pi';
 import { TbCalendarCheck, TbWorldCheck } from 'react-icons/tb';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+	const [tempCountry, setTempCountry] = useState('DE');
+	const [tempYear, setTempYear] = useState(2025);
+	const [countries, setCountries] = useState([]);
+	const [data, setData] = useState([]);
+
+	const listYear = [20, 21, 22, 23, 24, 25, 26];
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const [getHolidays, getCountries] = await Promise.all([
+					axios.get(
+						`https://openholidaysapi.org/PublicHolidays?countryIsoCode=${tempCountry}&validFrom=${tempYear}-01-01&validTo=${tempYear}-12-31&languageIsoCode=EN`,
+					),
+					axios.get('https://openholidaysapi.org/Countries'),
+				]);
+				setData(getHolidays.data);
+				setCountries(getCountries.data);
+			} catch (error) {
+				console.error('Error fetching holidays:', error);
+				setData([]);
+				setCountries([]);
+			}
+		};
+		fetchData();
+	}, [tempCountry, tempYear]);
 	return (
 		<div className='max-w-[360px] px-3 py-8 bg-white flex flex-col gap-4'>
 			{/* Title */}
@@ -14,10 +41,19 @@ function App() {
 			<form className='flex gap-3'>
 				{/* Selected Country */}
 				<div className='flex grow gap-2 justify-between p-2 border border-slate-300 items-center rounded-sm hover:bg-slate-100'>
-					<div className='flex grow gap-2 items-center'>
-						<TbWorldCheck className='text-slate-600' />
-						<select className='appearance-none'>
-							<option value=''>Netherlands (the)</option>
+					<div className='flex w-full gap-2 items-center'>
+						<TbWorldCheck className='text-slate-600 shrink-0' />
+						<select
+							value={tempCountry}
+							onChange={(e) => setTempCountry(e.target.value)}
+							className='w-full appearance-none'>
+							{countries.map((country, index) => {
+								return (
+									<option key={index} value={country.isoCode}>
+										{country.isoCode}
+									</option>
+								);
+							})}
 						</select>
 					</div>
 					<PiCaretDown className='text-slate-600' />
@@ -25,11 +61,20 @@ function App() {
 				{/* Selected Country - End */}
 
 				{/* Selected Year */}
-				<div className='w-[108px] flex gap-2 p-2 border border-slate-300 items-center rounded-sm hover:bg-slate-100'>
-					<div className='flex gap-2 items-center'>
-						<TbCalendarCheck className='text-slate-600 text-xl' />
-						<select name='' className='w-full appearance-none'>
-							<option value=''>2025</option>
+				<div className='w-[108px] justify-between flex gap-2 p-2 border border-slate-300 items-center rounded-sm hover:bg-slate-100'>
+					<div className='flex w-full gap-2 items-center'>
+						<TbCalendarCheck className='text-slate-600 shrink-0' />
+						<select
+							value={tempYear}
+							onChange={(e) => setTempYear(Number(e.target.value))}
+							className='w-full appearance-none'>
+							{listYear.map((year, index) => {
+								return (
+									<option key={index} value={2000 + year}>
+										{2000 + year}
+									</option>
+								);
+							})}
 						</select>
 					</div>
 					<PiCaretDown className='text-slate-600' />
@@ -47,10 +92,16 @@ function App() {
 					</tr>
 				</thead>
 				<tbody className='rounded-sm overflow-clip [&>tr]:odd:bg-slate-100 [&>tr]:hover:bg-sky-500'>
-					<tr className='[&>td]:px-2 [&>td]:py-2 cursor-pointer hover:text-white'>
-						<td>January 1</td>
-						<td className='font-bold'>News Year's Day</td>
-					</tr>
+					{data?.map((holiday, index) => {
+						return (
+							<tr
+								key={index}
+								className='[&>td]:px-2 [&>td]:py-2 cursor-pointer hover:text-white'>
+								<td>{holiday.startDate}</td>
+								<td className='font-bold'>{holiday.name[0].text}</td>
+							</tr>
+						);
+					})}
 				</tbody>
 			</table>
 			{/* Table - End */}
