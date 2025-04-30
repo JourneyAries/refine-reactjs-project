120bbf50cce072e66169e705bcb65b74e26ff1bf
JourneyAries <journeyaries13@gmail.com>
2025-04-30 15:14:52 +0700

    [public-holidasys-app](v2): before lifting state up

--- public-holidays-app-v2/src/api.jsx
+++ public-holidays-app-v2/src/api.jsx
@@ -4,3 +4,12 @@ export const getCountries = async () => {
 	const responses = await axios.get(`${import.meta.env.VITE_COUNTRIES}`);
 	return responses.data;
 };
+
+export const getHolidays = async () => {
+	const responses = await axios.get(
+		`${
+			import.meta.env.VITE_URL_BASE
+		}countryIsoCode=DE&validFrom=2023-01-01&validTo=2023-12-31&languageIsoCode=EN`,
+	);
+	console.info(responses.data);
+};
--- public-holidays-app-v2/src/components/SelectCountry.jsx
+++ public-holidays-app-v2/src/components/SelectCountry.jsx
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { PiCaretDown } from 'react-icons/pi';
 import { TbWorldCheck } from 'react-icons/tb';
-import { getCountries } from '../api';
+import { getCountries} from '../api';
 
 const SelectCountry = () => {
 	const [countries, setCountries] = useState([]);
+	const [selectedCode, setSelectedCode] = useState('');
 
 	useEffect(() => {
 		getCountries().then((res) => setCountries(res));
 	}, []);
+
+	const handleChange = (e) => {
+		const selectedValue = e.target.value;
+		setSelectedCode(selectedValue);
+		console.info(selectedValue);
+	};
 
 	return (
 		<div className='w-full relative gap-x-2 flex items-center'>
 			<TbWorldCheck className='absolute text-slate-600 left-2' />
 			<select
 				type='text'
-				onChange={() => {}}
+				value={selectedCode}
+				onChange={handleChange}
 				className='select-none w-full appearance-none pt-2 pr-8 pb-2 pl-[30px] border border-slate-300 text-slate-900 text-sm font-medium rounded cursor-pointer'>
 				{countries.map((country) => (
 					<option key={country.isoCode} value={country.isoCode}>
